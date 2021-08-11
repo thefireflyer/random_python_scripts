9a14a1bc01b63108704d4fc0bae9ae773d6cc3d4
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-08-10 17:39:17 -0700

    files and folders
    now can encrypt and decrypt an entire folder with one command

--- encryption_tests2.py
+++ encryption_tests2.py
@@ -8,11 +8,14 @@ parser.add_argument('--decrypt', help="dencrypts a file")
 args = parser.parse_args()
 
 
+
 password = str(args.password)
 m = hashlib.sha256()
 m.update(password.encode("utf-8"))
 key = base64.urlsafe_b64encode(m.digest())
 fernet = Fernet(key)
+
+
 
 def get_file_contents(path):
     f = codecs.open(path, 'rb')
@@ -23,24 +26,60 @@ def get_file_contents(path):
 
 
 if args.encrypt != None:
-    contents = get_file_contents(args.encrypt)
-    print("encrypting")
-    f = codecs.open(args.encrypt, 'wb')
+    
     try:
-        contents = fernet.encrypt(contents)
+        contents = get_file_contents(args.encrypt)
+        print("encrypting")
+        f = codecs.open(args.encrypt, 'wb')
+        try:
+            contents = fernet.encrypt(contents)
+        except:
+            print("failed to encrypt")
+        f.write(contents)
+        f.close()
+
     except:
-        print("failed to encrypt")
-    f.write(contents)
-    f.close()
+        for dirname, _, filenames in os.walk(str(args.encrypt)):
+            for filename in filenames:
+                #print(os.path.join(dirname, filename))
+                
+                contents = get_file_contents(os.path.join(dirname, filename))
+                print("encrypting")
+                f = codecs.open(os.path.join(dirname, filename), 'wb')
+                try:
+                    contents = fernet.encrypt(contents)
+                except:
+                    print("failed to encrypt")
+                f.write(contents)
+                f.close()
 
 if args.decrypt != None:
-    contents = get_file_contents(args.decrypt)
-    print("decrypting")
-    f = codecs.open(args.decrypt, 'wb')
+    
     try:
-        contents = fernet.decrypt(contents)
+        contents = get_file_contents(args.decrypt)
+        print("decrypting")
+        f = codecs.open(args.decrypt, 'wb')
+        try:
+            contents = fernet.decrypt(contents)
+        except:
+            print("failed to decrypt")
+        f.write(contents)
+        f.close()
+
     except:
-        print("failed to decrypt")
-    f.write(contents)
-    f.close()
+        for dirname, _, filenames in os.walk(str(args.decrypt)):
+            for filename in filenames:
+                #print(os.path.join(dirname, filename))
+                
+                contents = get_file_contents(os.path.join(dirname, filename))
+                print("decrypting")
+                f = codecs.open(os.path.join(dirname, filename), 'wb')
+                try:
+                    contents = fernet.decrypt(contents)
+                except:
+                    print("failed to decrypt")
+                f.write(contents)
+                f.close()
+
+
 
